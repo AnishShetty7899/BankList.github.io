4ff8130683fd506879005328c7315d5f0acad7ff
AnishShetty7899 <142916990+AnishShetty7899@users.noreply.github.com>
2023-08-24 18:24:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
 // DIFFERENT DATA! Contains movement dates, currency and locale
 
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'Anish Shetty',
   movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
@@ -30,7 +30,7 @@ const account1 = {
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
+  owner: 'Sowjanya Kulal',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
@@ -48,8 +48,42 @@ const account2 = {
   currency: 'USD',
   locale: 'en-US',
 };
+const account3 = {
+  owner: 'Shobha Shetty',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+   movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2021-04-01T10:17:24.185Z',
+    '2021-05-08T14:11:59.604Z',
+    '2023-07-07T17:01:17.194Z',
+    '2023-07-13T23:36:17.929Z',
+    '2023-07-14T10:51:36.790Z',
+  ],
+  currency: 'INR',
+  locale: 'pt-PT',
+};
 
-const accounts = [account1, account2];
+const account4 = {
+  owner: 'Nagayya Shetty',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+   movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2021-02-05T16:33:06.386Z',
+    
+  ],
+  currency: 'USD',
+  locale: 'en-US',
+};
+const accounts = [account1, account2,account3,account4];
 
 /////////////////////////////////////////////////
 // Elements
